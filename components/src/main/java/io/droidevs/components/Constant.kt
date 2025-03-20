68f69e89c940b792b11e99f17398c860fbb632c2
Mouad Oumous <mouadoumous@gmail.com>
2025-03-20 22:02:27 +0100

    add a constant components class to represent different constants in this calculator

---
+++ components/src/main/java/io/droidevs/components/Constant.kt
@@ -0,0 +1,18 @@
+package io.droidevs.components
+
+abstract class Constant(text : String) : Component(text = text) {
+
+    abstract fun apply() : Double
+
+    object PI : Constant("π") {
+        override fun apply(): Double {
+            return 3.14159265358979323846
+        }
+    }
+
+    object E : Constant("e") {
+        override fun apply(): Double {
+            return 2.71828182845904523536
+        }
+    }
+}
