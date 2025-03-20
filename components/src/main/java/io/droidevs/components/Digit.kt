402ed105dd5edcd4c60b33c7e19f562a64879a98
Mouad Oumous <mouadoumous@gmail.com>
2025-03-20 22:03:14 +0100

    add a digit components class to represent different digits in this calculator

---
+++ components/src/main/java/io/droidevs/components/Digit.kt
@@ -0,0 +1,25 @@
+package io.droidevs.components
+
+open class Digit(text: String) : Component(text) {
+
+    object Zero : Digit("0")
+
+    object One : Digit("1")
+
+    object Two : Digit("2")
+
+    object Three : Digit("3")
+
+    object Four : Digit("4")
+
+    object Five : Digit("5")
+
+    object Six : Digit("6")
+
+    object Seven : Digit("7")
+
+    object Eight : Digit("8")
+
+    object Nine : Digit("9")
+
+}
