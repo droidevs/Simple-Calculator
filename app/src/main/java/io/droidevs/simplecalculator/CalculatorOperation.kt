c3a67f69850f2e1e1e6025ef879040218b482d47
Mouad Oumous <mouadoumous@gmail.com>
2025-03-20 19:38:39 +0100

    add calculator operations

---
+++ app/src/main/java/io/droidevs/simplecalculator/CalculatorOperation.kt
@@ -0,0 +1,11 @@
+package io.droidevs.simplecalculator
+
+sealed class CalculatorOperation(
+    val symbol: String,
+) {
+
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+    object Multiply: CalculatorOperation("*")
+    object Divide: CalculatorOperation("/")
+}
